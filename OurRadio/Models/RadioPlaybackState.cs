8c62afe7535e6790ded38e4dcf41c8bea15c4e4a
FriendlyPigeon <levi.liukkonen@gmail.com>
2026-02-05 12:37:02 -0800

    Add a progress of current playing song for each Radio on the Radios page

--- OurRadio/Models/RadioPlaybackState.cs
+++ OurRadio/Models/RadioPlaybackState.cs
@@ -1,10 +1,12 @@
-namespace OurRadio.Models;
-
-public sealed class RadioPlaybackState
+namespace OurRadio.Models
 {
-    public int RadioId { get; set; }
-    public int SongId { get; set; }
-    public string Filename { get; set; } = string.Empty;
-    public int DurationSeconds { get; set; }
-    public DateTimeOffset StartedAtUtc { get; set; }
+    public sealed class RadioPlaybackState
+    {
+        public int RadioId { get; set; }
+        public int SongId { get; set; }
+        public string Filename { get; set; } = string.Empty;
+        public int DurationSeconds { get; set; }
+        public DateTimeOffset StartedAtUtc { get; set; }
+    }
 }
+
